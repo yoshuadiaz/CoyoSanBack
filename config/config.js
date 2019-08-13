f7caf562ceadc88c0608751337483810a3fb53af
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-13 00:55:19 -0500

    Initial Commit DB with Sequelize

---
+++ config/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "dbcoyosan",
+    "host": "127.0.0.1",
+    "dialect": "sqlite",
+    storage: 'dbcoyosan.sqlite',
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "dbcoyosan",
+    "host": "127.0.0.1",
+    "dialect": "sqlite",
+    storage: 'dbcoyosan.sqlite',
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "dbcoyosan",
+    "host": "127.0.0.1",
+    "dialect": "sqlite",
+    storage: 'dbcoyosan.sqlite',
+    "operatorsAliases": false
+  }
+}

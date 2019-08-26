d0c0cad4b0f4e414f3945640f5299688be1ec5f6
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-08-26 00:09:28 -0500

    Update all

--- server.js
+++ server.js
@@ -2,23 +2,21 @@ require("dotenv").config()
 const bodyParser = require("body-parser")
 const db = require("./db/models")
 
-// db.sequelize.sync()
-
 const express = require("express")
 const app = express()
 
-// const SenseiTypesRouter = require("./routes/sensei_types")
-// const UserRouter = require("./routes/user")
-// const GoalRouter = require("./routes/goal")
-// const SavingRouter = require("./routes/saving")
-//const sensei_health = require("./controlers/sensei_health");
+const SenseiTypesRouter = require("./routes/sensei_types")
+const UserRouter = require("./routes/user")
+const GoalRouter = require("./routes/goal")
+const SavingRouter = require("./routes/saving")
+
 app.set("port", process.env.PORT || 3000)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use("/sensei-types", SenseiTypesRouter)
-// app.use("/user", UserRouter)
-// app.use("/goal", GoalRouter)
-// app.use("/saving", SavingRouter)
+app.use("/sensei-types", SenseiTypesRouter)
+app.use("/user", UserRouter)
+app.use("/goal", GoalRouter)
+app.use("/saving", SavingRouter)
 app.get("/", function(req, res) {
   res.send("CoyoSan App")
   res.end()
